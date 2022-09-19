d5d81ef3c5d78454f8408680f86eaf5fd924f1ef
josgrehd <josgrehd@gmail.com>
2022-09-19 17:18:50 +0000

    Excusas terminadas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["my dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function who1(max = 3, min = 0) {
+  var randomNumber = Math.floor(Math.random() * (max + 1 - min) + min);
+  return parseInt(randomNumber);
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function what1(max = 3, min = 0) {
+  var randomNumber = Math.floor(Math.random() * (max + 1 - min) + min);
+  return parseInt(randomNumber);
+}
+
+function when1(max = 4, min = 0) {
+  var randomNumber = Math.floor(Math.random() * (max + 1 - min) + min);
+  return parseInt(randomNumber);
+}
+
+function onLoad() {
+  var excusa1 = who[who1()];
+  var excusa2 = what[what1()];
+  var excusa3 = when[when1()];
+  var espacio = " ";
+  var cadena = excusa1 + espacio + excusa2 + espacio + excusa3;
+  return cadena;
+}
